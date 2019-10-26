f27ac91d65fbbc1560cb981dd74694892be3c99e
Georgiy Demo <demenchuk.george@protonmail.com>
2019-10-26 15:14:52 +0300

    Скелет для малины
    Необходимо отредачить compose + #1

---
+++ server/server.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, send_file
+from flask_restful import Resource, Api
+import picamera
+import time
+import uuid
+
+
+app = Flask(__name__)
+api = Api(app)
+
+class PhotoProcessing():
+    def __init__(self):
+        self.result = False
+        self.file_name = str(uuid.uuid1())+".jpg"
+        self.processing()
+
+    def processing(self):
+
+        with picamera.PiCamera() as camera:
+            camera.rotation = 180
+            camera.resolution = (1024, 768)
+            camera.start_preview()
+            time.sleep(2)
+            camera.capture(self.file_name)
+        self.result = True
+
+
+class GetPhoto(Resource):
+    def get(self):
+        """
+        Метод для получения фото с камеры Raspberry Pi
+        """
+        obj = PhotoProcessing()
+        if obj.result == True:
+            return send_file(obj.file_name, mimetype='image/png')
+        else:
+            return {"status": "exception"}
+
+api.add_resource(GetPhoto, '/get_photo')
+
+if __name__ == '__main__':
+    app.run(host='192.168.5.171', debug=False)
