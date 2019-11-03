e3d4ab6e0d1c9464a62c4c9d34bb59237a211de8
Georgiy Demo <demenchuk.george@protonmail.com>
2019-11-03 14:35:59 +0300

    Без записи на sd карту работаем

--- server/server.py
+++ server/server.py
@@ -6,36 +6,37 @@ import time
 import redis
 import base64
 
+import numpy as np
+import io
+
 r_redis = redis.Redis(host='redis', port=6379, db=1)
 app = Flask(__name__)
 api = Api(app)
 
-
 def get_photo():
     while True:
         PhotoProcessing()
-        time.sleep(3)
+        time.sleep(1)
 
 class PhotoProcessing():
     def __init__(self):
-        self.result = False
         self.file_name = "image.jpg"
         self.processing()
         self.redis_writer()
 
     def processing(self):
+        stream = io.BytesIO()
         with picamera.PiCamera() as camera:
             camera.rotation = 180
             camera.resolution = (1024, 768)
             camera.start_preview()
             time.sleep(2)
-            camera.capture(self.file_name)
-        self.result = True
+            camera.capture(stream, format='jpeg')
+        stream.seek(0)
+        self.data = stream
     
     def redis_writer(self):
-
-        with open(self.file_name, "rb") as image_file:
-            encoded_string = base64.b64encode(image_file.read())
+        encoded_string = base64.b64encode(self.data)
         r_redis.set("img", encoded_string)
 
 class GetPhoto(Resource):
@@ -45,9 +46,9 @@ class GetPhoto(Resource):
         """
         try:
             file_base64 = bytes(r_redis.get("img"))
-            with open("img.jpg", "wb") as f:
-                f.write(base64.decodebytes(file_base64))
-            return send_file("img.jpg", mimetype='image/jpg')
+            in_memory_file = io.BytesIO(file_base64)
+            data = np.frombuffer(in_memory_file.getvalue(), dtype=np.uint8)
+            return send_file(data, mimetype='image/jpg')
         except:
             return {"status": "exception"}
 
